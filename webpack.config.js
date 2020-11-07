aec1b011eea4c0d265e19bc28c1fbed4180ec3aa
Ludmilishee <luda_babich@mail.ru>
2020-11-07 21:15:49 +0500

    proj-1: add webpack, impement hmr, add less

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        port: 8000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Emoji Widget',
+        }),
+    ],
+};
