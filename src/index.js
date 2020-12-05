a8c65a1fb9791676c9acd9dd4fffab077b585f5b
Ludmilishee <luda_babich@mail.ru>
2020-12-05 21:17:49 +0500

    self-render: add widget self rendering

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import './styles/index.styl';
 import { Widget } from "./widget";
 
-const widgetButton = document.getElementById('widget-button');
-const widgetContent = document.getElementById('widget-content');
+const messageInputEl = document.getElementById('messageInput');
+const widget = new Widget(messageInputEl);
 
-widgetButton.onclick = function () {
-    setWidgetVisibility();
-};
-const widget = new Widget();
-
-function setWidgetVisibility() {
-    const hiddenWidgetClass = 'widget-hidden';
-    if (widgetContent.classList.contains(hiddenWidgetClass)) {
-        widgetContent.classList.remove(hiddenWidgetClass);
-    } else {
-        widgetContent.classList.add(hiddenWidgetClass);
-    }
-}
